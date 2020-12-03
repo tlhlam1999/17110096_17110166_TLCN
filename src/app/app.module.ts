3ec33fe500f1ffeb7a6238dc408c17dae58ab664
tlhlam1999 <66105436+tlhlam1999@users.noreply.github.com>
2020-12-04 02:12:06 +0700

    update screen declare

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,10 @@ import { OwlModule } from 'ngx-owl-carousel';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
+
 import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 @NgModule({
   declarations: [
@@ -49,7 +53,10 @@ import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
     OwlModule,
     BrowserAnimationsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    MatSelectModule
   ],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   providers: [],
